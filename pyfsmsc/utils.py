672e386e208466575be259d4c739ff9a900b9b32
Will Ferguson <wferguso@andrew.cmu.edu>
2024-04-09 23:57:18 +0000

    black formatting

--- pyfsmsc/utils.py
+++ pyfsmsc/utils.py
@@ -1,18 +1,20 @@
-import requests 
-from collections.abc import Iterable 
+import requests
+from collections.abc import Iterable
 
 import numpy as np
 
+
 def hello(name):
-    print(f'Welcome to pyfsmsc: {name}')
+    print(f"Welcome to pyfsmsc: {name}")
+
 
 def RDF_to_SQ(r, gr, density, qmin, qmax, nqs):
-    rs = r;
-    dr = r[1] - r[0];
-    
+    rs = r
+    dr = r[1] - r[0]
+
     RDFs = gr - 1
 
-    qs = np.linspace(qmin, qmax, num = nqs)
+    qs = np.linspace(qmin, qmax, num=nqs)
     Sqs = np.zeros(qs.shape[0])
 
     for qind, q in enumerate(qs):
@@ -20,5 +22,5 @@ def RDF_to_SQ(r, gr, density, qmin, qmax, nqs):
         for rind, r in enumerate(rs):
             s += dr * r * np.sin(q * r) * RDFs[rind]
             Sqs[qind] = 1 + 4 * np.pi * density * s / q
-            
+
     return qs, Sqs
