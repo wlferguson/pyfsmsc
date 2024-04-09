fb5076616981accd889622581a938a6422a5bf2f
Will Ferguson <wferguso@andrew.cmu.edu>
2024-04-09 22:27:10 +0000

    Creating package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyfsmsc',
+      version='0.0.1',
+      description='A fluids and soft materials structure characterization package',
+      maintainer='William Ferguson',
+      maintainer_email='wferguso@andrew.cmu.edu',
+      license='MIT',
+      packages=['RDF','FT_sQ','sQ','ShapeMetrics'],
+      long_description='''This package
+      includes utilties to characterize the structures of simulated fluids and soft materials.''')
