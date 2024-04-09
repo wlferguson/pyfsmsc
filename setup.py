72dbad4557401d457e4edf38233529f60dfc98ae
Will Ferguson <wferguso@andrew.cmu.edu>
2024-04-09 22:50:18 +0000

    Updated setup file.

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(name='pyfsmsc',
       maintainer='William Ferguson',
       maintainer_email='wferguso@andrew.cmu.edu',
       license='MIT',
-      packages=['RDF','FT_sQ','sQ','ShapeMetrics'],
       long_description='''This package
       includes utilties to characterize the structures of simulated fluids and soft materials.''')
