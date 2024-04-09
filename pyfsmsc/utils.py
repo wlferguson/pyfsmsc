a871ab6f4b97038232525a79f683fde7fe35b759
Will Ferguson <wferguso@andrew.cmu.edu>
2024-04-09 23:51:58 +0000

    First structure calculation features, F.T.-RDF

--- pyfsmsc/utils.py
+++ pyfsmsc/utils.py
@@ -1,28 +1,24 @@
 import requests 
 from collections.abc import Iterable 
 
+import numpy as np
+
 def hello(name):
-    print(f'Hey there {name}')
+    print(f'Welcome to pyfsmsc: {name}')
 
 def RDF_to_SQ(r, gr, density, qmin, qmax, nqs):
     rs = r;
     dr = r[1] - r[0];
     
-    #RDFs = gr - np.ones_like(rs)
+    RDFs = gr - 1
 
-    #qs = np.linspace(qmin, qmax, num = nqs)
-    #Sqs = np.zeros_like(qs)
+    qs = np.linspace(qmin, qmax, num = nqs)
+    Sqs = np.zeros(qs.shape[0])
 
-    #for qind, q in enumerate(qs):
-    #    s = 0
-    #    for rind, r in enumerate(rs):
-    #    s += dr * r * np.sin(q * r) * RDFs[rind]
-    #    Sqs[qind] = 1 + 4 * np.pi * density * s / q  
-    return dr
-
-
-
-    
-    
-    
-    
+    for qind, q in enumerate(qs):
+        s = 0
+        for rind, r in enumerate(rs):
+            s += dr * r * np.sin(q * r) * RDFs[rind]
+            Sqs[qind] = 1 + 4 * np.pi * density * s / q
+            
+    return qs, Sqs
