1b809cc557110d2557d773007da86ccd19e7c74d
William Ferguson <wferguso@andrew.cmu.edu>
2024-04-12 14:14:55 -0400

    Added requirements and updated setup.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyfsmsc",
-    version="0.0.3",
+    version="1.0.0",
     description="A fluids and soft materials structure characterization package",
     maintainer="William Ferguson",
     maintainer_email="wferguso@andrew.cmu.edu",
