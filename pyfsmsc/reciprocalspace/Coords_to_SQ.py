f1876e17f25bd055a27c7d14599b046d48f3c836
William Ferguson <wferguso@andrew.cmu.edu>
2024-04-14 12:10:21 -0400

    Function and structure changes to Coord to SQ.

--- pyfsmsc/reciprocalspace/Coords_to_SQ.py
+++ pyfsmsc/reciprocalspace/Coords_to_SQ.py
@@ -1,15 +1,14 @@
-"""Include utilities for calculating scattering information in reciprocal space."""
+"""Include utilities for calculating reciprocal space scattering from coordinates."""
 
 import numpy as np
 from numba import jit
 import pandas as pd
-from scipy.signal import savgol_filter
 import warnings
 
 warnings.filterwarnings("ignore")
 
 
-def Coords_to_SQ(nmax, L, df) -> tuple:
+def Coords_to_SQ(nmax, L, df):
     """Convert atomic coordinates into scattering data.
 
     Parameters
@@ -28,9 +27,39 @@ def Coords_to_SQ(nmax, L, df) -> tuple:
     ds3 : ndarray
         1D array containing non-averaged scattering data, S(q), of simulation in `float` type.
     """
+
+    ds1 = generateWaves(nmax, L)
+    ds3 = waveInteractions(ds1, df)
+    ds3 = ds3 / df.shape[0]
+    qmagVec = np.zeros((nmax**3, 1))
+    # for rows in range(0, nmax**3):
+    #    qmagVec[rows] = (ds1[rows][0] ** 2 + ds1[rows][1] ** 2 + ds1[rows][2] ** 2) ** (
+    #        0.5
+    #    )
+
+    qmagVec = np.linalg.norm(ds1, axis=1)
+
+    return qmagVec, ds3
+
+
+def generateWaves(nmax, L):
+    """Create compatible scattering vectors for the system.
+
+    Parameters
+    ----------
+    nmax : int
+        Maximum integer index for reciprocal vector scaling.
+    L : float
+        Size of the periodic simulation box.
+
+    Returns
+    -------
+    ds1 : ndarray
+        1D array containing allowable scattering vectors for system geometry.
+    """
+
     nk = nmax
-    kCard = (nk) ** 3
-    ds1 = np.zeros((kCard, 3))
+    ds1 = np.zeros(((nk) ** 3, 3))
     index = 0
 
     for nx in range(0, nk):
@@ -41,42 +70,36 @@ def Coords_to_SQ(nmax, L, df) -> tuple:
                 ds1[index][2] = nz
                 index += 1
     ds1 = 2 * np.pi / L * ds1
-    ds3 = waveInteractions(ds1, df)
-    ds3 = ds3 / df.shape[0]
-    qmagVec = np.zeros((kCard, 1))
-    for rows in range(0, kCard):
-        qmagVec[rows] = (ds1[rows][0] ** 2 + ds1[rows][1] ** 2 + ds1[rows][2] ** 2) ** (
-            0.5
-        )
 
-    return qmagVec, ds3
+    return ds1
 
 
 @jit(nopython=True)
-def waveInteractions(A, B) -> np.ndarray:
-    """Perform rigorous calculations of wave calculations with JIT compilation.
+def waveInteractions(q, C):
+    """Perform wave interaction with particle calculation with JIT compilation.
 
     Parameters
     ----------
-    A : ndarray
-        2D array containing atomic coordinates of simulation in `float` type.
-    B : float
+    q : ndarray
         2D array containing allowable q vectors of simulation box in `float` type.
+    C : float
+        2D array containing atomic coordinates of simulation in `float` type.
 
     Returns
     -------
     ds3 : ndarray
         1D array containing non-averaged scattering data, S(q), of simulation in `float` type.
     """
-    ds3 = np.zeros((A.shape[0], 1))
+    ds3 = np.zeros((q.shape[0], 1))
     sumInit1 = 0
     sumInit2 = 0
-    for row in range(0, A.shape[0]):
-        for i in range(0, B.shape[0]):
-            sumInit1 = sumInit1 + np.cos(np.dot(A[row], B[i]))
-            sumInit2 = sumInit2 + np.sin(np.dot(A[row], B[i]))
+    for row in range(0, q.shape[0]):
+        for i in range(0, C.shape[0]):
+            sumInit1 = sumInit1 + np.cos(np.dot(q[row], C[i]))
+            sumInit2 = sumInit2 + np.sin(np.dot(q[row], C[i]))
         tot = sumInit1**2 + sumInit2**2
         ds3[row] = tot
         sumInit1 = 0
         sumInit2 = 0
+
     return ds3
